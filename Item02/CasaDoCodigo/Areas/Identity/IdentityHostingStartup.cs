73f7052b33e322bc00268921b00292423b8a4f80
marcelooliveira <mclricardo@gmail.com>
2019-05-23 13:23:33 -0300

    preparando para o vídeo

--- Item02/CasaDoCodigo/Areas/Identity/IdentityHostingStartup.cs
+++ Item02/CasaDoCodigo/Areas/Identity/IdentityHostingStartup.cs
@@ -11,6 +11,19 @@ using Microsoft.Extensions.DependencyInjection;
 [assembly: HostingStartup(typeof(CasaDoCodigo.Areas.Identity.IdentityHostingStartup))]
 namespace CasaDoCodigo.Areas.Identity
 {
+    //PRECISAMOS TRADUZIR AS MENSAGENS DE ERRO DE SENHAS:
+
+    //ORIGINAL:
+    //=========
+    //Passwords must have at least one non alphanumeric character.
+    //Passwords must have at least one lowercase('a' - 'z').
+    //Passwords must have at least one uppercase('A' - 'Z').
+
+    //PT-BR:
+    //=========
+    //A senha deve ter pelo menos um caractere alfanumérico.
+    //A senha deve ter pelo menos uma letra minúscula('a' - 'z').
+    //A senha deve ter pelo menos uma letra minúscula('a' - 'z').
     public class IdentityHostingStartup : IHostingStartup
     {
         public void Configure(IWebHostBuilder builder)
