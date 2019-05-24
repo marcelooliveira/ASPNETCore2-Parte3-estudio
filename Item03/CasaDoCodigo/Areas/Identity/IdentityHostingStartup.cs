d5e86401505138d0b6aea919edf117850c64eb22
marcelooliveira <mclricardo@gmail.com>
2019-05-24 14:05:52 -0300

    Revert "1203-AspNetCoreParte3Identity-video2.6-Adicionando Nome de Usuário.trec"
    This reverts commit 5eabfbf60baaa00ae781560b119694c812aa672b.

--- Item03/CasaDoCodigo/Areas/Identity/IdentityHostingStartup.cs
+++ Item03/CasaDoCodigo/Areas/Identity/IdentityHostingStartup.cs
@@ -45,8 +45,6 @@ namespace CasaDoCodigo.Areas.Identity
                         options.Password.RequireNonAlphanumeric = 
                         options.Password.RequireLowercase = 
                         options.Password.RequireUppercase = false;
-
-                        options.User.AllowedUserNameCharacters += ' ';
                     })
                     .AddErrorDescriber<IdentityErrorDescriberPtBr>()
                     .AddEntityFrameworkStores<AppIdentityContext>();
